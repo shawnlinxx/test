1df170948c6849d8934bd38cdc509e4d960d4f14
shawnlinxx <shawn.x.lin@ericsson.com>
2015-10-12 09:53:07 +0100

    12

--- Q2012.java
+++ Q2012.java
@@ -1,1 +1,28 @@
-sushu
+import java.util.Scanner;
+public class Prime {
+	public static boolean isPrime(int num) {
+		for(int i = 2;i <= Math.sqrt(num);i++) {
+			if(num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Pls enter the first number");
+		int x = sc.nextInt();
+		System.out.println("Pls enter the second number");
+		int y = sc.nextInt();
+		if(x == 0 && y == 0)
+			break;
+		else {
+			for(int i = x;i <= y; i++) {
+				if(Prime.isPrime(i) != 1)
+				       System.out.println("Sorry!");
+				}
+			 System.out.println("OK!");
+		}
+	}
+}
